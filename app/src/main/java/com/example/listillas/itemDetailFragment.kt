4edea77a3f6bda5b0def9b82e3f1219e3597c9bf
dasil <dasilvadiego.inf@gmail.com>
2022-07-01 13:51:13 +0200

    feat: add cancel logic

--- app/src/main/java/com/example/listillas/itemDetailFragment.kt
+++ app/src/main/java/com/example/listillas/itemDetailFragment.kt
@@ -1,10 +1,14 @@
 package com.example.listillas
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
 
 class itemDetailFragment : Fragment() {
 
@@ -13,6 +17,34 @@ class itemDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_item_detail, container, false)
+        val view = inflater.inflate(R.layout.fragment_item_detail, container, false)
+
+        val saveButton = view.findViewById<Button>(R.id.saveItemButton)
+        val deleteButton = view.findViewById<Button>(R.id.deleteItemButton)
+        val cancelButton = view.findViewById<Button>(R.id.cancelItemButton)
+
+        saveButton.setOnClickListener { saveButtonHandler(view) }
+        deleteButton.setOnClickListener { deleteButtonHandler(view) }
+        cancelButton.setOnClickListener { cancelButtonHandler(view) }
+
+        return view
+    }
+
+    fun saveButtonHandler(view: View) {
+
+    }
+
+    fun deleteButtonHandler(view: View) {
+        Log.d("ItemDetail", "Click on delete button: ")
+    }
+    fun cancelButtonHandler(view: View) {
+        val title = view.findViewById<TextInputEditText>(R.id.titleField)
+        val description = view.findViewById<TextInputEditText>(R.id.descriptionField)
+
+        title.setText("")
+        description.setText("")
+
+
+
     }
 }
